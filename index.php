f4e2ec3fd8b6e57b2e6e11cf85a51ba88b1548d0
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-01-08 14:02:47 +0100

    .

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$str = "Hello World";
+var_dump($str);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Movie ratings</title>
+</head>
+<body>
+  <h1>My Movie ratings</h1>
+</body>
+</html>
